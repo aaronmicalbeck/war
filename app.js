0254bad5a8a0d8c1dc06d3ec5efd5bdc585a5dd0
Aaron Beck <aaronmicalbeck@gmail.com>
2021-08-12 19:09:10 -0400

    players draw and cards are checked

--- app.js
+++ app.js
@@ -2,20 +2,22 @@
 console.log('jQuery is on!');
 
 // randomly generates unique numbers between 1 and 52 and stores them in an array, splits the array in half and deals to each player
-function ShuffleAndDeal() {
+function PlayGame() {
 
-    console.log("Shuffle!");
+    console.log("Shuffled and Dealt!");
 
 
     let deck = [];
     let playerOneHand = [];
     let playerTwoHand = [];
+    let playerOneMove = 0;
+    let playerTwoMove = 0;
 
     while (deck.length < 52) {
         let cards = Math.floor(Math.random() * 52) + 1;
         if (deck.indexOf(cards) === -1) deck.push(cards);
     }
-    
+
 
     let half = Math.ceil(deck.length / 2);
 
@@ -25,49 +27,79 @@ function ShuffleAndDeal() {
     console.log(playerOneHand);
     console.log(playerTwoHand);
 
-    
+    // removes last index from each player's hand
+    function Draw() {
+
+        console.log("DRAW!");
+
+        let p1Move = playerOneHand.pop();
+        let p2Move = playerTwoHand.pop();
+
+        console.log("Player One's move = " + p1Move);
+        console.log("Player Two's move = " + p2Move);
+        CheckCards();
+
+    function CheckCards() {
+
+        console.log("Checking Cards!");
+
+            if (p1Move > p2Move) {
+                console.log("Player 1 Wins!");
+                PlayerOneTakeCards();
+            }
+            else if (p1Move < p2Move) {
+                console.log("Player 2 Wins!")
+            }
+
+            else {
+                console.log("WAR!!!")
+            }
+
+
+
+        };
+
+
+
+
+    };
+
+    // compares each player's move
+
+
+    // adds cards to first index of winning player's hand
+    function PlayerOneTakeCards() {
+
+
+        console.log("Take those cards!");
+    };
+    function PlayerTwoTakeCards() {
+
+
+        console.log("Take those cards!");
+    };
+
+
+
+    $("#drawBtn").click(function () {
+        Draw();
+    })
+
+
+
+    $("#takeCardsBtn").click(function () {
+        TakeCards();
+    })
+
+
 
 
 };
 
-
-
-// removes last index from each player's hand
-function Draw() {
-
-
-    console.log("DRAW!");
-
-};
-
-
-// compares each player's move
-function CheckCards() {
-
-
-    console.log("Check Cards!");
-};
-
-// adds cards to first index of winning player's hand
-function TakeCards() {
-
-
-    console.log("Take those cards!");
-};
-
-
-// Here is where the magic happens!
-
-
-
-$("#drawBtn").click(function () {
-    Draw();
+$("#playBtn").click(function () {
+    PlayGame();
 })
 
-$("#shuffleBtn").click(function () {
-    ShuffleAndDeal();
-})
 
-$("#takeCardsBtn").click(function () {
-    TakeCards();
-})
+
+
