8fc8dd455bed168747b8c4359c4ed223b1c2a9db
Aaron Beck <aaronmicalbeck@gmail.com>
2021-08-12 20:05:54 -0400

    winning draw takes card, game can be won

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ console.log('jQuery is on!');
 // randomly generates unique numbers between 1 and 52 and stores them in an array, splits the array in half and deals to each player
 function PlayGame() {
 
-    console.log("Shuffled and Dealt!");
-
 
     let deck = [];
     let playerOneHand = [];
     let playerTwoHand = [];
-    let playerOneMove = 0;
-    let playerTwoMove = 0;
+    
 
     while (deck.length < 52) {
         let cards = Math.floor(Math.random() * 52) + 1;
@@ -24,34 +21,41 @@ function PlayGame() {
     playerOneHand = deck.slice(0, half);
     playerTwoHand = deck.slice(-half);
 
-    console.log(playerOneHand);
-    console.log(playerTwoHand);
+    console.log(`P1 = ${playerOneHand}`);
+    console.log(`P2 = ${playerTwoHand}`);
 
     // removes last index from each player's hand
     function Draw() {
-
-        console.log("DRAW!");
 
         let p1Move = playerOneHand.pop();
         let p2Move = playerTwoHand.pop();
 
         console.log("Player One's move = " + p1Move);
         console.log("Player Two's move = " + p2Move);
+        
         CheckCards();
+
+        console.log(`Player Two has ${playerOneHand.length} cards.`);
+        console.log(`Player Two has ${playerTwoHand.length} cards.`)
+        console.log(`P1 = ${playerOneHand}`);
+        console.log(`P2 = ${playerTwoHand}`);
 
     function CheckCards() {
 
         console.log("Checking Cards!");
 
             if (p1Move > p2Move) {
-                console.log("Player 1 Wins!");
-                PlayerOneTakeCards();
+                console.log("Player One Wins!");
+                playerOneHand.unshift(p1Move, p2Move)
+               
             }
             else if (p1Move < p2Move) {
-                console.log("Player 2 Wins!")
+                console.log("Player Two Wins!")
+                playerTwoHand.unshift(p2Move, p1Move);
+                
             }
 
-            else {
+            else if (p1Move == p2Move) {
                 console.log("WAR!!!")
             }
 
