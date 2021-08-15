1d96c7f64ed547af01ec2e6f530a6ad0d8a39406
Aaron Beck <aaronmicalbeck@gmail.com>
2021-08-15 12:19:38 -0400

    cleaned

--- app.js
+++ app.js
@@ -3,87 +3,98 @@ console.log('jQuery is on!');
 
 const suits = ["spades", "hearts", "clubs", "diamonds"];
 const values = ["14", "13", "12", "11", "10", "9", "8", "7", "6", "5", "4", "3", "2",];
-let deck = new Array();
 
-    
 
-    function BuildDeck() {
-        let deck = new Array();
-        // iterates over suits and values arrays and creates a new deck array of objects
-        for (let i = 0; i < suits.length; i++) {
-            for (let x = 0; x < values.length; x++) {
-                let card = { Value: values[x], Suit: suits[i] };
-                deck.push(card);
-            }
+
+
+function BuildDeck() {
+    let deck = new Array();
+    // iterates over suits and values arrays and creates a new deck array of objects
+    for (let i = 0; i < suits.length; i++) {
+        for (let x = 0; x < values.length; x++) {
+            let card = { Value: values[x], Suit: suits[i] };
+            deck.push(card);
         }
+    }
 
-        // iterates over the new deck and switches the location of two random indices 1000 times
-        for (let i = 0; i < 1000; i++) {
-            let location1 = Math.floor((Math.random() * deck.length));
-            let location2 = Math.floor((Math.random() * deck.length));
-            let tmp = deck[location1];
+    // iterates over the new deck and switches the location of two random indices 1000 times
+    for (let i = 0; i < 1000; i++) {
+        let location1 = Math.floor((Math.random() * deck.length));
+        let location2 = Math.floor((Math.random() * deck.length));
+        let tmp = deck[location1];
 
-            deck[location1] = deck[location2];
-            deck[location2] = tmp;
-        }
+        deck[location1] = deck[location2];
+        deck[location2] = tmp;
+    }
 
-        // splits deck array into 2 and pushes to players' hands
+    return deck;
+
+}
+
+    // splits deck array into 2 and pushes to players' hands
+
+    function Deal(){
+
+        let deck = BuildDeck();
 
         let half = Math.ceil(deck.length / 2);
 
         let playerOneHand = deck.slice(0, half);
         let playerTwoHand = deck.slice(-half);
-
+    
         for (const key in playerOneHand) {
             console.log("P1 = " + key, playerOneHand[key]);
         }
         for (const key in playerTwoHand) {
             console.log("P2 = " + key, playerTwoHand[key]);
         }
+
     }
 
+    
 
 
 
 
-    function Draw() { // removes last index from each player's hand
-
-        let p1Move = playerOneHand.pop();
-        let p2Move = playerTwoHand.pop();
-
-        console.log("Player One's move = " + p1Move);
-        console.log("Player Two's move = " + p2Move);
-
-        CheckCards();
-
-        console.log(`Player Two has ${playerOneHand.length} cards.`);
-        console.log(`Player Two has ${playerTwoHand.length} cards.`)
-        console.log(`P1 = ${playerOneHand}`);
-        console.log(`P2 = ${playerTwoHand}`);
-
-        function CheckCards() { // compares each player's move & adds cards to first index of winning player's hand
-
-            console.log("Checking Cards!");
-
-            if (p1Move > p2Move) {
-                console.log("Player One Wins!");
-                playerOneHand.unshift(p1Move, p2Move)
-
-            }
-            else if (p1Move < p2Move) {
-                console.log("Player Two Wins!")
-                playerTwoHand.unshift(p2Move, p1Move);
-
-            }
-
-            else if (p1Move == p2Move) {
-                console.log("WAR!!!")
-            }
 
 
 
-        };
 
+
+
+function Draw() { // removes last index from each player's hand
+
+    let p1Move = playerOneHand.pop();
+    let p2Move = playerTwoHand.pop();
+
+    console.log("Player One's move = " + p1Move);
+    console.log("Player Two's move = " + p2Move);
+
+    CheckCards();
+
+    console.log(`Player Two has ${playerOneHand.length} cards.`);
+    console.log(`Player Two has ${playerTwoHand.length} cards.`)
+    console.log(`P1 = ${playerOneHand}`);
+    console.log(`P2 = ${playerTwoHand}`);
+
+    function CheckCards() { // compares each player's move & adds cards to first index of winning player's hand
+
+        console.log("Checking Cards!");
+
+        if (p1Move > p2Move) {
+            console.log("Player One Wins!");
+            playerOneHand.unshift(p1Move, p2Move)
+
+        }
+        else if (p1Move < p2Move) {
+            console.log("Player Two Wins!")
+            playerTwoHand.unshift(p2Move, p1Move);
+
+        }
+
+        else if (p1Move == p2Move) {
+            console.log("WAR!!!")
+        }
 
 
 
@@ -91,24 +102,36 @@ let deck = new Array();
 
 
 
-    $("#drawBtn").click(function () {
-        Draw();
-    })
 
-
-
-    $("#takeCardsBtn").click(function () {
-        TakeCards();
-    })
-
-
-
+};
 
 
 
 $("#playBtn").click(function () {
     BuildDeck();
 })
+
+$("#dealBtn").click(function(){
+    Deal();
+})
+
+
+$("#drawBtn").click(function () {
+    Draw();
+})
+
+
+
+$("#takeCardsBtn").click(function () {
+    TakeCards();
+})
+
+
+
+
+
+
+
 
 
 
